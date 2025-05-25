33702a7eada073efc8b61ca6e87925c68a8cd898
yanghao5 <yanghao.buffer@outlook.com>
2025-05-26 02:13:01 +0800

    [feat](githubaction) add githubaction.py

---
+++ githubaction.py
@@ -0,0 +1,19 @@
+import sys
+import os 
+
+# internal pkg
+from validator import toml
+
+def get_providers():
+    providers = os.getenv('PROVIDERS')
+    cleaned_providers = providers.replace('\r', '')
+    if providers:
+        with open('providers.toml', 'w') as f:
+            f.write(providers)
+        print("Providers.toml has been created successfully.")
+    else:
+        print("No PROVIDERS environment variable found.")
+
+if __name__ == "__main__":
+    get_providers()
+    toml.validate('providers.toml')
