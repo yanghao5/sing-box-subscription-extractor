be9392c86f005ad649862accbe23c0f762969197
yanghao5 <yanghao.buffer@outlook.com>
2025-05-29 15:35:17 +0800

    [feat](githubaction) update

--- githubaction.py
+++ githubaction.py
@@ -44,17 +44,8 @@ if __name__ == "__main__":
     # Step 7 gen
     generator.gen(subs,p)
     
-    # Step 9 get old nodes
-    is_upload=False
+    # Step 8 upload to cloudflare kv  
     if p.cloudflare:
-        oldnodes=cf.get(p.cloudflare,"nodes")
-        with open("./temp/oldnodes.json", 'w') as json_file:
-            json.dump(oldnodes, json_file, indent=4, ensure_ascii=False)  # 将字典写入文件，并格式化为 4 个空格的缩进
-        print(f"oldnodes data successfully written to ./temp/oldnodes.json")
-        
-    
-    # Step 10 upload to cloudflare kv  
-    if is_upload:
         # upload nodes
         with open("./temp/nodes.json", "r", encoding="utf-8") as file:
             data = json.load(file)
